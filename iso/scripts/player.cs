09ae7d88415ae7600a7a4e1257df6951fc7a655e
Novascone <novasconewarren@gmail.com>
2024-02-04 15:56:46 -0700

    added enemy detection with ray casting

--- iso/scripts/player.cs
+++ iso/scripts/player.cs
@@ -11,20 +11,23 @@ public partial class player : CharacterBody3D
 	private AnimationPlayer animation_player; // animation player
 	private Area3D hitbox; // weapon hitbox
 	// Called when the node enters the scene tree for the first time.
+	private Area3D target;
 	public override void _Ready()
 	{
 		navigation_agent = (NavigationAgent3D)GetNode("NavigationAgent3D"); // get reference to NavigationAgent3D
 		animation_player = (AnimationPlayer)GetNode("AnimationPlayer"); // get reference to AnimationPlater
 		hitbox = (Area3D)GetNode("WeaponPivot/WeaponMesh/Hitbox"); // get reference to Hitbox
 		hitbox.AreaEntered += OnHitboxAreaEntered; // subscribe hitbox to OnHitboxAreaEntered signal
-		
 	}
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
 
     public override void _Process(double delta)
 	{
-		attack_check();
+		if(attack_check())
+		{
+			look_at_point(delta);
+		}
 
 		if(navigation_agent.IsNavigationFinished())
 		{
@@ -32,6 +35,8 @@ public partial class player : CharacterBody3D
 		}
 		
 		move_to_point(delta, speed); // moves character to point
+		
+		
 
 	}
 
@@ -44,6 +49,13 @@ public partial class player : CharacterBody3D
 		MoveAndSlide(); // Godot physics
 	}
 
+	public void look_at_point(double delta)
+	{
+		var target_position = navigation_agent.GetNextPathPosition(); // use the information taken from the _Input function to set the target position
+		face_direction(target_position); // face direction moving
+		// GD.Print("look");
+	}
+
 	
 
 	public void face_direction(Vector3 direction)
@@ -51,10 +63,14 @@ public partial class player : CharacterBody3D
 		LookAt(direction with { Y = GlobalPosition.Y }, Vector3.Up); // rotates character to face direction moving
 	}
 
+	
+
+
+
 	public override void _Input(InputEvent @event) // function to move character
 	{
 		
-		if (Input.IsActionPressed("LeftMouse"))
+		if ((Input.IsActionPressed("LeftMouse")) | (Input.IsActionJustPressed("Attack")))
 		{
 			Camera3D camera = (Camera3D)GetTree().GetNodesInGroup("Camera")[0]; // get reference to camera
 			Vector2 mouse_pos = GetViewport().GetMousePosition(); // get 2d mouse position
@@ -69,30 +85,34 @@ public partial class player : CharacterBody3D
 			var result = space.IntersectRay(ray_query); // get location and information of where the ray went
 			/* result can be used to get information about what you are clicking on, for instance if you are clicking on an enemy you can use get the parent of
 			   the collider to detect if it is in fact and enemy.
-			GD.Print(result);
 			*/ 
-
 			navigation_agent.TargetPosition = result["position"].AsVector3(); // setting navigation agent target position
-
+			GodotObject obj = result["collider"].AsGodotObject();
+			// GD.Print(result["collider"]);
+			check_obj_clicked(obj); // check object collided with
+			
 		}
-		
+	
 	}
 
-	public void attack_check() // changes weapon hitbox monitoring based on animation
+	public bool attack_check() // changes weapon hitbox monitoring based on animation
 	{
 		if(Input.IsActionPressed("Attack"))
 		{
 			animation_player.Play("attack");
 			hitbox.Monitoring = true;
-
+			return true;
 		}
 		
-		if(Input.IsActionJustReleased("Attack"))
+		else if(Input.IsActionJustReleased("Attack"))
 		{
 			hitbox.Monitoring = false;
-
+			return false;
 		}
+
+		return false;
 	}
+
 
 	private void OnHitboxAreaEntered(Area3D area_hit) // handler for area entered signal
 	{
@@ -100,6 +120,18 @@ public partial class player : CharacterBody3D
 		{
 			GD.Print("enemy hit");
 		}
+	}
+
+	private void check_obj_clicked(GodotObject obj) // checks to see what group the object click on is, needs ray cast results dictionary results["collider"]
+	{
+		if(obj is Area3D) // checks if object from results["collider"] is an Area3D
+			{
+				Area3D area = (Area3D)obj; // casts it as an Area3D
+				if(area.IsInGroup("enemy")) // checks group
+				{
+					GD.Print("enemy");
+				}
+			}
 	}
 
 
